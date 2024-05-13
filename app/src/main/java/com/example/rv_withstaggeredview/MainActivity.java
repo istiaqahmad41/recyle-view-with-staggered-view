2707555b9c98ea287c6e9d1380c105ad07171368
Istiaq Ahmad <istiaqahmad41@gmail.com>
2024-05-13 16:51:12 +0600

    commited

---
+++ app/src/main/java/com/example/rv_withstaggeredview/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.rv_withstaggeredview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private static final String TAG = "MainActivity_4";
+    private static final int NUM_COLUMNS = 1;
+
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<String> mNames = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initImageBitmaps();
+    }
+
+    private void initImageBitmaps(){
+        // Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
+        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mNames.add("Havasu Falls");
+
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mNames.add("Trondheim");
+
+        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+        mNames.add("Portugal");
+
+        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
+        mNames.add("Rocky Mountain National Park");
+
+
+        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+        mNames.add("Mahahual");
+
+        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
+        mNames.add("Frozen Lake");
+
+
+        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
+        mNames.add("White Sands Desert");
+
+        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
+        mNames.add("Austrailia");
+
+        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
+        mNames.add("Washington");
+
+        initRecyclerView();
+
+    }
+
+    private void initRecyclerView(){
+        // Log.d(TAG, "initRecyclerView: initializing staggered recyclerview.");
+
+        RecyclerView recyclerView = findViewById(R.id.recyleView);
+
+        RV1_StaggeredRecyclerViewAdapter_4 staggeredRecyclerViewAdapter4 =
+                new RV1_StaggeredRecyclerViewAdapter_4(this, mNames, mImageUrls);
+
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(NUM_COLUMNS, LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+
+
+
+        recyclerView.setAdapter(staggeredRecyclerViewAdapter4);
+    }
+}
---
+++ app/src/main/java/com/example/rv_withstaggeredview/RV1_StaggeredRecyclerViewAdapter_4.java
@@ -0,0 +1,79 @@
+package com.example.rv_withstaggeredview;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+import com.example.rv_withstaggeredview.R;
+public class RV1_StaggeredRecyclerViewAdapter_4 extends RecyclerView.Adapter<RV1_StaggeredRecyclerViewAdapter_4.ViewHolder> {
+
+    private static final String TAG = "StaggeredRecyclerViewAd";
+
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private Context mContext;
+
+    public RV1_StaggeredRecyclerViewAdapter_4(Context context, ArrayList<String> names, ArrayList<String> imageUrls) {
+        mNames = names;
+        mImageUrls = imageUrls;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv1_layout_grid_item_4, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background);
+
+        Glide.with(mContext)
+                .load(mImageUrls.get(position))
+                .apply(requestOptions)
+                .into(holder.image);
+
+        holder.name.setText(mNames.get(position));
+
+        /*holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on: " + mNames.get(position));
+                Toast.makeText(mContext, mNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageUrls.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView name;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.image = itemView.findViewById(R.id.imageview_widget);
+            this.name = itemView.findViewById(R.id.name_widget);
+        }
+    }
+}
